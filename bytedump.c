fe56386b2545750d690b2c3142c6dc089577a267
Manu-sh <s3gmentationfaul@gmail.com>
2018-01-11 14:22:46 +0100

    fix extra-padding

--- bytedump.c
+++ bytedump.c
@@ -4,7 +4,19 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-#define BSIZE 8192
+/* 
+
+   a good choice for the size of buffer:
+   avoid printing extra padding you should align the buffer size according to chunk size for example for chunk of 5 bytes at times
+   8192 isn't a good value but you could use 8000 for example, here there is a little example of what was intended for extra-padding
+
+   | . . . . H | 176 000 000 000 072 | 260 000 000 000 110 | b0 00 00 00 48 |
+   | . 5       | 139 053             | 213 065             | 8b 35          | // should be appear only on the last line
+   | . / . . H | 188 047 032 000 072 | 274 057 040 000 110 | bc 2f 20 00 48 |
+
+*/
+
+#define BSIZE 8000
 #define STREAM_READ stdin
 #define STREAM_WRITE stdout
 
@@ -54,6 +66,7 @@ int main() {
 
 	fprintf(STREAM_WRITE, "| ASCII     | INT %17c OCT %17c HEX %12c\n", '|', '|', '|');
 	while ((rb = fread(buf, 1, BSIZE, STREAM_READ)) != 0) {
+
 		for (int i = 0, n; i < rb; i+=5) {
 			n = (5 > rb-i ? rb-i : 5); fprintf(STREAM_WRITE, "| ");
 
